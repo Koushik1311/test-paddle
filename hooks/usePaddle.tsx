e304885e964d38d829a1c3bc1683ca16052e3a7b
Koushik <programmer.koushik@gmail.com>
2024-06-17 23:18:20 +0530

    init

---
+++ hooks/usePaddle.tsx
@@ -0,0 +1,27 @@
+// hooks/usePaddle.tsx
+"use client";
+import {
+  initializePaddle,
+  InitializePaddleOptions,
+  Paddle,
+} from "@paddle/paddle-js";
+import { useEffect, useState } from "react";
+
+export default function usePaddle() {
+  const [paddle, setPaddle] = useState<Paddle>();
+  useEffect(() => {
+    initializePaddle({
+      environment: process.env.PADDLE_ENV! ? "production" : "sandbox",
+      token: process.env.PADDLE_CLIENT_TOKEN!,
+      seller: 20459,
+    } as unknown as InitializePaddleOptions).then(
+      (paddleInstance: Paddle | undefined) => {
+        if (paddleInstance) {
+          setPaddle(paddleInstance);
+        }
+      }
+    );
+  }, []);
+
+  return paddle;
+}
